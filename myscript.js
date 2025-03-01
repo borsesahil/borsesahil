493e1dea00d2178086998ce3f05100e458b913d5
borsesahil <sahil.borse2004@gmail.com>
2025-03-01 11:54:53 +1000

    Car Showroom Sample Webpage

---
+++ myscript.js
@@ -0,0 +1,9 @@
+let i=0;
+i=i++;
+console.log("here is my first console log output."+i);
+i=++i;
+console.log("here is my first console log output."+i);
+i=++i;
+console.log("here is my first console log output."+i);
+i=++i;
+console.log("here is my first console log output."+i);
